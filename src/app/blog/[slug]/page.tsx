8f820f50bfc35bf11111068b5ed3d82317d3aa63
martin-harley <mharley007@gmail.com>
2026-03-14 13:25:12 -0700

    Implement conditional rendering in blog post page to display a 'Coming Soon' message when no content is available. Introduce BlogComingSoon component for better user experience.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from 'next/navigation'
 import { client } from '@/sanity/lib/client'
 import { BLOG_POST_QUERY } from '@/sanity/lib/queries'
 import BlogPostPage from '@/components/blog/BlogPostPage'
+import BlogComingSoon from '@/components/blog/BlogComingSoon'
 import type { BlogPost } from '../../../../sanity.types'
 
 export default async function Page({
@@ -14,5 +15,7 @@ export default async function Page({
 
   if (!post) notFound()
 
-  return <BlogPostPage post={post} />
+  const hasBody = post.body && post.body.length > 0
+
+  return hasBody ? <BlogPostPage post={post} /> : <BlogComingSoon />
 }
